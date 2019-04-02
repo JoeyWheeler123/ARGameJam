4eebe08984a96dd99d7b909225eda19ec1967569
gazor117 <oreillygary7@gmail.com>
2019-04-02 13:53:44 +0100

    Enemy Stuff Added

---
+++ ARGameJam/Assets/EnemyManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+   
+
+    public List<GameObject> Enemys = new List<GameObject>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
